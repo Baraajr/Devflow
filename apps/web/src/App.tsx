b40566f0f11d4af3b5223a653d9691498ad4de38
Baraa <ahmedbaraa009@gmail.com>
2026-08-31 18:51:31 +0300

    Added landing page with utils

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,38 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import LandingPage from './pages/LandingPage';
+import ProtectedRoute from './ui/ProtectedRoute';
+import PublicRoute from './ui/PublicRoute';
+import { Toaster } from './ui/Toaster';
+import Applayout from './ui/Applayout';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        {/* Public routes */}
+        <Route element={<PublicRoute />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Applayout />}>
+            <Route path="/dashboard" element={<div>dashboard</div>} />
+          </Route>
+        </Route>
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      <Toaster />
+    </>
+  );
 }
 
 export default App;

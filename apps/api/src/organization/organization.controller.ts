c0f2138529eea389d07e13be8fb7ecbb92eac2c4
Baraa <ahmedbaraa009@gmail.com>
2026-09-17 20:37:57 +0300

    feat: get organization members leave organization and delete organization

--- apps/api/src/organization/organization.controller.ts
+++ apps/api/src/organization/organization.controller.ts
@@ -1,35 +1,70 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+
 import { Organization } from './entities/organization.entity';
 import { CreateOrgDto } from './dto/create-org.dto';
 import { OrganizationService } from './organization.service';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
-import { User } from '../users/entities/user.entity';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { User } from '../users/entities/user.entity';
 
 @ApiTags('Organizations')
+@UseGuards(JwtAuthGuard)
 @Controller('organization')
 export class OrganizationController {
   constructor(private readonly organizationService: OrganizationService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   async create(
     @CurrentUser() user: User,
     @Body() dto: CreateOrgDto,
   ): Promise<Organization> {
-    const organization = await this.organizationService.create(dto, user.id);
-
-    return organization;
+    return this.organizationService.create(dto, user.id);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   async getMyOrganizations(@CurrentUser() user: User) {
-    const organizations = await this.organizationService.getUserOrganizations(
-      user.id,
-    );
+    return this.organizationService.getUserOrganizations(user.id);
+  }
 
-    return organizations;
+  @Get(':organizationId')
+  async getOrg(
+    @Param('organizationId', ParseUUIDPipe) organizationId: string,
+    @CurrentUser() user: User,
+  ): Promise<Organization> {
+    return this.organizationService.getOrganization(user.id, organizationId);
+  }
+
+  @Get(':organizationId/members')
+  async getMembers(
+    @Param('organizationId', ParseUUIDPipe) organizationId: string,
+    @CurrentUser() user: User,
+  ) {
+    return this.organizationService.getOrgMembers(user.id, organizationId);
+  }
+
+  @Delete(':organizationId')
+  async deleteOrganization(
+    @Param('organizationId', ParseUUIDPipe) organizationId: string,
+    @CurrentUser() user: User,
+  ): Promise<void> {
+    return this.organizationService.deleteOrganization(user.id, organizationId);
+  }
+
+  @Delete(':organizationId/members/me')
+  async leaveOrganization(
+    @Param('organizationId', ParseUUIDPipe) organizationId: string,
+    @CurrentUser() user: User,
+  ): Promise<void> {
+    return this.organizationService.leaveOrganization(user.id, organizationId);
   }
 }

afb8bfda0e36d05e4657dace466dde4eac83c28f
Baraa <ahmedbaraa009@gmail.com>
2026-09-19 15:18:48 +0300

    feat: email service

---
+++ apps/api/src/mail/mail.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailService } from './mail.service';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('mail.host'),
+          port: config.get<number>('mail.port'),
+          secure: false,
+          auth: {
+            user: config.get<string>('mail.username'),
+            pass: config.get<string>('mail.password'),
+          },
+        },
+        defaults: {
+          from: config.get<string>('mail.from'),
+        },
+      }),
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
